052241248089fc47f50f072a2aa68bc36c216400
Onur Erdem <erdem.onur@outlook.com.tr>
2023-10-31 12:49:32 +0300

    Ödev 31.10.2023

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     home: Scaffold(
-      backgroundColor: Color.fromARGB(255, 70, 4, 105),
+      backgroundColor: const Color.fromARGB(255, 70, 4, 105),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
+            Image.asset(
+              'assets/2120418120210530115238453.jpg',
+              height: 150.0,
+              fit: BoxFit.cover,
+            ),
+            const Text(
               'Onur Erdem',
               style: TextStyle(fontSize: 40, color: Colors.white),
             ),
-            Text(
+            const Text(
               'Mobil Geliştirici (Flutter) - 1A',
               style: TextStyle(fontSize: 25, color: Colors.white),
             ),
-            Text(
+            const Text(
               '31.10.2023',
               style: TextStyle(fontSize: 20, color: Colors.white),
             ),
